04a93a75f6ec11c321cfa1efb4336b3fbcbe8e8b
Bheb Developer <bhebdeveloper@gmail.com>
2022-07-05 20:58:53 +0530

    05/07/2022

---
+++ bankLoan.java
@@ -0,0 +1,65 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class bankLoan {
+    public static void main(String[] args) {
+
+        int accNo, accBal, custSal, loanAmt, emi;
+
+        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter ACCOUNT NUMBER");
+        accNo = scan.nextInt();
+
+        System.out.println("Enter CUSTOMER SALARY");
+        custSal = scan.nextInt();
+
+        System.out.println("Enter ACCOUNT BALANCE");
+        accBal = scan.nextInt();
+
+        System.out.println("Enter LOAN TYPE 'Car', 'House', 'Business'");
+        String loanType = sc.nextLine();
+
+        int len = String.valueOf(accNo).length();
+
+        String pos = Integer.toString(accNo);
+
+        if (len == 4 && pos.charAt(0) == '1') {
+
+            if (accBal >= 1000) {
+
+                if (custSal > 25000 && custSal < 50000 && Objects.equals(loanType, "Car")) {
+                    loanAmt = 500000;
+                    emi = 36;
+                    System.out.println("Eligible Loan Amount = " + loanAmt);
+                    System.out.println("Eligible EMIs = " + emi);
+
+                } else if (custSal > 50000 && custSal < 75000 && Objects.equals(loanType, "House")) {
+                    loanAmt = 600000;
+                    emi = 60;
+                    System.out.println("Eligible Loan Amount = " + loanAmt);
+                    System.out.println("Eligible EMIs = " + emi);
+
+                } else if (custSal > 75000 && Objects.equals(loanType, "Business")) {
+                    loanAmt = 750000;
+                    emi = 84;
+                    System.out.println("Eligible Loan Amount = " + loanAmt);
+                    System.out.println("Eligible EMIs = " + emi);
+
+                } else {
+                    System.out.println("Not ELIGIBLE  for LOAN");
+                }
+
+            } else {
+                System.out.println("Insufficient Balance");
+            }
+        } else {
+            System.out.println("Invalid ID!!!");
+        }
+
+        scan.close();
+        sc.close();
+    }
+
+}
